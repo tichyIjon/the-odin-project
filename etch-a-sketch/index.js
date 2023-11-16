0556bdcb62b1d72a97e346aedd14d5a6d5573ad8
tichyIjon <anon420@disroot.org>
2023-11-16 20:50:31 +0100

    feat: add grid resize buttons

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,15 +1,43 @@
 const container = document.querySelector('.container')
+let dimensions;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div')
-    square.setAttribute('class','sqr')
-    square.setAttribute('style','border: 1px solid black; height: 40px; width: 40px; margin: 1px;')
-    container.append(square)
+function generateGrid(dimensions) {
+    document.querySelector('.container').innerHTML = '';
+
+    let height = (512/(Math.sqrt(dimensions)));
+    let width = (512/(Math.sqrt(dimensions)));
+
+    for (let i = 0; i < dimensions; i++) {
+        let square = document.createElement('div')
+        square.setAttribute('class','sqr')
+        square.setAttribute('style',`border: 0.5px dashed lightgrey; height: ${height}px; width: ${width}px;`)
+        container.append(square)
+    }
 }
 
-let sqr = document.querySelectorAll('.sqr')
-sqr.forEach(box => {
+function generateEventListeners() {
+    let sqr = document.querySelectorAll('.sqr')
+    sqr.forEach(box => {
     box.addEventListener('mouseover',()=> {
         box.style.backgroundColor = 'red';
     })
 })
+}
+
+const btn16 = document.querySelector('.sizeOne').addEventListener('click',()=> {
+    generateGrid(256);
+    generateEventListeners()
+})
+const btn32 = document.querySelector('.sizeTwo').addEventListener('click',()=> {
+    generateGrid(1024)
+    generateEventListeners()
+})
+const btn64 = document.querySelector('.sizeThree').addEventListener('click',()=> {
+    generateGrid(4096)
+    generateEventListeners()
+})
+const btn128 = document.querySelector('.sizeFour').addEventListener('click',()=> {
+    generateGrid(16384)
+    generateEventListeners()
+})
+
