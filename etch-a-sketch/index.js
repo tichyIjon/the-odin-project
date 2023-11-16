7eab6a42f81dd39c7a59cd2133e84365a08968ee
tichyIjon <anon420@disroot.org>
2023-11-16 19:15:48 +0100

    feat: set up a hover trail

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -2,7 +2,14 @@ const container = document.querySelector('.container')
 
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div')
-    square.setAttribute('style','border: 1px solid black; height: 40px; width: 40px; margin: 1px')
+    square.setAttribute('class','sqr')
+    square.setAttribute('style','border: 1px solid black; height: 40px; width: 40px; margin: 1px;')
     container.append(square)
 }
 
+let sqr = document.querySelectorAll('.sqr')
+sqr.forEach(box => {
+    box.addEventListener('mouseover',()=> {
+        box.style.backgroundColor = 'red';
+    })
+})
